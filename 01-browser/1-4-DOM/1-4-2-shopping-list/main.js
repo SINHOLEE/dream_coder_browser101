1a4ca632602acc581192668228557b1b48ab8922
이신호 <dltlsgh5@naver.com>
2021-02-12 19:16:33 +0900

    shopping list by sinho

---
+++ 01-browser/1-4-DOM/1-4-2-shopping-list/main.js
@@ -0,0 +1,60 @@
+function init() {
+	const input = document.querySelector('input');
+	const form = document.querySelector('form');
+	const section = document.querySelector('section');
+	const plusBtn = document.querySelector('.icon');
+
+	// utilize 가능
+	const getItemDivOrNull = (elem) => {
+		if (elem.classList.contains('item')) {
+			return elem;
+		}
+		if (elem.parentNode === null) {
+			return null;
+		}
+		return getItemDivOrNull(elem.parentNode);
+	};
+
+	const removeBy = (item) => {
+		item.remove();
+	};
+	const clickRemoveBtn = (e) => {
+		const targetItem = getItemDivOrNull(e.target);
+		removeBy(targetItem);
+	};
+
+	const addItem = (item) => {
+		const div = document.createElement('div');
+		div.setAttribute('class', 'item');
+
+		const spanLeft = document.createElement('span');
+		spanLeft.setAttribute('class', 'item-text');
+		spanLeft.textContent = `${item}`;
+
+		const spanRight = document.createElement('span');
+		spanRight.setAttribute('class', 'trash');
+		spanRight.innerHTML = '<i class="far fa-trash-alt"></i>';
+
+		div.appendChild(spanLeft);
+		div.appendChild(spanRight);
+
+		// add event remove
+		// 이벤트 위임으로 성능 향상 가능
+		spanRight.addEventListener('click', clickRemoveBtn);
+		section.appendChild(div);
+	};
+
+	const submit = (e) => {
+		e.preventDefault();
+		const value = input.value;
+		if (!value) return;
+
+		addItem(value);
+		input.value = '';
+	};
+
+	form.addEventListener('submit', submit);
+	plusBtn.addEventListener('click', submit);
+}
+
+init();
